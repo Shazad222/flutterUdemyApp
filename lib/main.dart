8206427d0d448d86c9fe449555e2e95258041b83
shahzad <shahzaddaw@shahzads-MacBook-Pro.local>
2024-10-20 00:16:23 -0700

    Initial commit with login screen

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'view/screens/authentication_screen/Login_screen.dart';
+
+void main() {
+  runApp(PdfViewerApp());
+}
+
+class PdfViewerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(), // Calling the LoginScreen here
+    );
+  }
+}
